a92a8b793693f2002de1dd872535ea55ea48b447
kayellas <zeynepmlfl@gmail.com>
2023-12-17 20:54:23 +0300

    login

---
+++ login.php
@@ -0,0 +1,42 @@
+<?php
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    //retrieve from data
+    $kullanici_adi = $_POST['kullanici_adi'];
+    $password = $_POST['sifre'];
+}
+    //database connection
+
+    $host = "localhost";
+    $dbUsername = "root";
+    $dbpassword = "";
+    $dbName = "personel_takip";
+
+
+    $conn = new mysqli($host, $dbUsername, $dbpassword, $dbName );
+
+    if($conn-> connect_error){
+        die("Bağlantı Hatası: ".$conn-> connect_error);
+    }
+
+    //validate login authentiction
+    $query = "SELECT * FROM kullanici WHERE kullanici_adi='$kullanici_adi' AND sifre='$password'";
+    $result = $conn->query($query);
+
+    if($result->num_rows ==1 ){
+        //login success
+        header("Location: home.html");
+    }
+    else{
+        //login failed
+        header("Location: error.html");
+        exit();
+    }
+    $conn->close();
+
+
+
+
+
+?>
+
